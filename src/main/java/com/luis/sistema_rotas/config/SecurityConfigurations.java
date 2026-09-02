5bb3caa03e314d7f673fcbecde180bbb6ac0f025
LuisGustav18 <luis0gustavo43@gmail.com>
2026-09-02 13:54:30 -0300

    Adiciona configuração de cookies dentro de SecurityConfigurations

--- src/main/java/com/luis/sistema_rotas/config/SecurityConfigurations.java
+++ src/main/java/com/luis/sistema_rotas/config/SecurityConfigurations.java
@@ -55,6 +55,7 @@ public class SecurityConfigurations {
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
